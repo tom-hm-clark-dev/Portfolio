43c416c189700168209ac2bb76e6171bbfe8a613
Tom Clark <tom.hm.clark@gmail.com>
2026-06-14 20:55:14 +0100

    Created interface for Projects

---
+++ src/types/types.ts
@@ -0,0 +1,16 @@
+export interface Project {
+    slug: string;
+    platform: string;
+    title: string;
+    subtitle?: string;
+    excerpt: string;
+    url: string;
+    hero?: string;
+    images?: string[];
+    roles?: string[];
+    tech?: string[];
+    content?:  {
+        type: string;
+        text: string;
+    }
+}
